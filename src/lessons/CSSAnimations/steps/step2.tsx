fc87fc734a26e835b3f01130b4b9a5eb188a5417
kacperkapusciak <kacper.kapusciak@swmansion.com>
2025-05-23 16:09:59 +0200

    Add step2

---
+++ src/lessons/CSSAnimations/steps/step2.tsx
@@ -0,0 +1,78 @@
+import { AppIcon } from "@/components/AppIcon";
+import { apps } from "@/lib/apps";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  CSSAnimationKeyframes,
+  runOnJS,
+} from "react-native-reanimated";
+
+const shake: CSSAnimationKeyframes = {
+  from: {
+    transform: [{ rotateZ: "2deg" }],
+  },
+  "15%": {
+    transform: [{ rotateZ: "-2deg" }],
+  },
+  "30%": {
+    transform: [{ rotateZ: "2deg" }],
+  },
+  "45%": {
+    transform: [{ rotateZ: "-2deg" }],
+  },
+  "60%": {
+    transform: [{ rotateZ: "2deg" }],
+  },
+  "75%": {
+    transform: [{ rotateZ: "-2deg" }],
+  },
+  to: {
+    transform: [{ rotateZ: "0deg" }],
+  },
+};
+
+export function CSSAnimationsLesson() {
+  const [isEditMode, setEditMode] = useState(false);
+
+  const longPress = Gesture.LongPress().onStart(() => {
+    runOnJS(setEditMode)(true);
+  });
+
+  const tap = Gesture.Tap().onStart(() => {
+    runOnJS(setEditMode)(false);
+  });
+
+  const composed = Gesture.Exclusive(longPress, tap);
+
+  return (
+    <View style={styles.container}>
+      <GestureDetector gesture={composed}>
+        <Animated.View
+          style={[
+            {
+              animationName: shake,
+              animationDuration: 700,
+              animationIterationCount: "infinite",
+              animationPlayState: isEditMode ? "running" : "paused",
+            },
+          ]}
+        >
+          <AppIcon app={apps[0]} />
+        </Animated.View>
+      </GestureDetector>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    [process.env.EXPO_OS === "web"
+      ? "backgroundImage"
+      : "experimental_backgroundImage"]:
+      "linear-gradient(180deg,rgba(125, 211, 252, 1) 0%, rgba(29, 78, 216, 1) 100%)",
+  },
+});
